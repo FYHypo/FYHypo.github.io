5fe47c277819e03551739d40098aeab3063764f0
fyhypo <2733961879@qq.com>
2024-09-01 17:33:11 +0800

    Site updated: 2024-09-01 17:33:10

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
-//const live2d_path = "http://47.109.68.178/blog-static/live2d-widget/";
-const live2d_path = "/live2d-widget/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/FYHypo/FYHypo.github.io@main/live2d-widget/";
+//const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
